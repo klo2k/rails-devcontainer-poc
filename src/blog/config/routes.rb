912535f911e206cffb04868517d9eccb633a6731
Kok-Yan Lo <kok-yan.lo@mq.edu.au>
2021-11-16 14:34:36 +1100

    Tutorial: Generate link to article using helper method

--- src/blog/config/routes.rb
+++ src/blog/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "/debugtest", to: "debugtest#index"
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # Auto-generate CRUD routes for "/articles" urls
+  resources :articles
 end
